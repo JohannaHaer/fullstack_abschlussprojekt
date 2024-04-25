526b669ffb6b07458ff84c3e7a36a3276b0ffc2a
JohannaHaer <johanna.haerdter@gmail.com>
2024-04-25 15:36:32 +0200

    toggle

--- frontend/src/pages/SetupAccount.jsx
+++ frontend/src/pages/SetupAccount.jsx
@@ -1,12 +1,10 @@
 import SetupAccountForm from '@/components/SetupAccountForm'
 import React from 'react'
-import Toggle from '@/components/Toggle'
 
 const SetupAccount = () => {
     return (
         <section className='flex flex-col align-center text-center h-dvh gap-5 p-5'>
             <SetupAccountForm/>
-            <Toggle/>
         </section>
     )
 }

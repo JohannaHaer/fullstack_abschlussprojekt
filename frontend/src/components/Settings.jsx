f205de507f9ecb10ee37318fadaeb5e4241606a0
JohannaHaer <johanna.haerdter@gmail.com>
2024-04-28 21:52:17 +0200

    Logo in settings eingebunden

--- frontend/src/components/Settings.jsx
+++ frontend/src/components/Settings.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Header'
 import NavBar from './NavBar'
+import LogoNormal from './logo/LogoNormal'
 
 const Settings = () => {
     return (
         <>
+        <LogoNormal/>
         <section className='py-10 px-4 h-dvh'>
             <Header/>
             <h1>Settings</h1>

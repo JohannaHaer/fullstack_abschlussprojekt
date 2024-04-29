36fb0a2d4d4ef599271fabb3a2bcf9772244371b
VictordelaM <delamotte.v@gmail.com>
2024-04-29 16:09:18 +0200

    image upload mit cloudinary

---
+++ backend/user/userController/uploadImage.js
@@ -0,0 +1,23 @@
+import { uploadImage } from "../../utils/imgUpload.js";
+import { User } from "../userModel/user.model.js";
+import { userRouter } from "./userLogin.js";
+
+
+
+export const imageUpload = async (req,res) =>{
+    console.log('check')
+    try{
+        // const username = jwt.decode(req.cookies.token).username
+        const username = 'red2'
+        const uploadResult = await uploadImage(req.file.buffer)
+        const imgUrl = uploadResult.secure_url
+        const user = await User.findOneAndUpdate(
+            {username:username},
+            {pictureUrl: imgUrl}
+        )
+        res.json(user)
+    }catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
--- backend/user/userRoutes/user.routes.js
+++ backend/user/userRoutes/user.routes.js
@@ -9,19 +9,14 @@ import { addExpense } from "../userController/addExpense.js";
 import { addIncome } from "../userController/addIncome.js";
 import { addExpenseCategory } from "../userController/addExpenseCategory.js";
 import { addIncomeCategory } from "../userController/addIncomeCategory.js";
-import { v2 as cloudinary } from "cloudinary";
 import { checkRepeatEmail, checkRepeatName } from "../../middleware/checkRepeat.js";
 import { editUser } from "../userController/editUser.js";
 import { changePassword } from "../userController/changePassword.js";
+import { imageUpload } from "../userController/uploadImage.js";
 
 const userRouter = express.Router()
 const mult = multer({ storage: multer.memoryStorage() })
 
-cloudinary.config({
-    cloud_name: "dvuefu0mx",
-    api_key: "736547154696513",
-    api_secret: process.env.CLOUDINARY_SECRET,
-  });
 
 userRouter.post("/register",mult.none(),checkRepeatName, checkRepeatEmail, register)
 userRouter.post('/login', mult.none(), login)
@@ -31,7 +26,8 @@ userRouter.post('/addExpense', mult.none(), checkAuth, addExpense)
 userRouter.post('/addIncome', mult.none(), checkAuth, addIncome)
 userRouter.post('/addExpenseCategory', mult.none(), checkAuth, addExpenseCategory)
 userRouter.post('/addIncomeCategory', mult.none(), checkAuth, addIncomeCategory)
-userRouter.patch('/editUser', mult.none(),checkAuth, editUser)
+userRouter.patch('/editUser', mult.none(), checkAuth, editUser)
 userRouter.patch('/changePassword', mult.none(), checkAuth, changePassword)
+userRouter.patch('/uploadImage', mult.single("image"), checkAuth, imageUpload) //sowohl für initialen upload als auch als änderung
 
 export default userRouter

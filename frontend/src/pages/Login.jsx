7762b23f94d510ee30ce254f4001a1f2b5692587
JohannaHaer <johanna.haerdter@gmail.com>
2024-04-30 08:56:47 +0200

    margin h1 register und login entfernt

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
         <>
             <LogoNormal/>
             <section className='flex flex-col align-center text-center h-dvh gap-5 p-5'>
-                <h1 className='text-center text-2xl font-bold mt-10'>Welcome back</h1>
+                <h1 className='text-center text-2xl font-bold'>Welcome back</h1>
                 <p className='text-center'>Please log in to access your account.</p>
                 <LoginForm/>
                 <p className='text-lg mt-5'>Don't have an account? <span onClick={navigateRegister} className='text-[#06434E] dark:text-[#FFDE59] font-bold'>Register</span></p>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -14,7 +14,7 @@ const Register = () => {
         <>
             <LogoNormal/>
             <section className='flex flex-col align-center text-center h-dvh gap-5 p-5'>
-                <h1 className='text-center text-2xl font-bold mt-10'>Create an Account</h1>
+                <h1 className='text-center text-2xl font-bold'>Create an Account</h1>
                 <p className='text-center'>Fill out the following form to create your account.</p>
                 <RegisterForm/>
                 <p className='text-lg mt-5'>Already have an Account? <span onClick={navigateLogin} className='text-[#06434E] dark:text-[#FFDE59] font-bold'>Login</span></p>

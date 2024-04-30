0a0feda644bcd827915aa392dfdcdd6f240d94e2
JohannaHaer <johanna.haerdter@gmail.com>
2024-04-30 12:02:05 +0200

    flowbite entfernt

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,7 +12,6 @@ import Header from '@/components/Header'
 import NavBar from '@/components/NavBar'
 import LogoNormal from '@/components/logo/LogoNormal'
 import { mainContext } from '@/context/mainProvider'
-import AreaChart from '@/components/flowbite-components/AreaChart'
 
 
 const Home = () => {
@@ -24,7 +23,6 @@ const Home = () => {
         <LogoNormal/>
         <section className='py-5 px-4 h-dvh'>
             <Header/>
-            <AreaChart/>
             <Card className="py-14 h-dvh w-full " >
                 <CardHeader className="h-3/6 w-full flex flex-col gap-12">
                     <Card className=" bg-accent rounded-lg h-3/6 " >

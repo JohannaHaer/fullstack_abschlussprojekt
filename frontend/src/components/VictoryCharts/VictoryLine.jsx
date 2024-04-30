60b8e4595bf9a3c3824ea906eec45144dfddaae9
JohannaHaer <johanna.haerdter@gmail.com>
2024-04-30 23:59:21 +0200

    logos behoben darkmode systems und pie chart farblich angepasst

--- frontend/src/components/VictoryCharts/VictoryLine.jsx
+++ frontend/src/components/VictoryCharts/VictoryLine.jsx
@@ -6,22 +6,22 @@ const VictoryLine = () => {
     return (
         <>
             <VictoryChart
-                theme={VictoryTheme.grayscale}
-            >
-            <VictoryLine
-                style={{
-                data: { stroke: "#c43a31" },
-                parent: { border: "1px solid #ccc"}
-                }}
-              data={[
-                { x: 1, y: 2 },
-                { x: 2, y: 3 },
-                { x: 3, y: 5 },
-                { x: 4, y: 4 },
-                { x: 5, y: 7 }
-              ]}
-            />
-            </VictoryChart>
+  theme={VictoryTheme.material}
+>
+  <VictoryLine
+    style={{
+      data: { stroke: "#c43a31" },
+      parent: { border: "1px solid #ccc"}
+    }}
+    data={[
+      { x: 1, y: 2 },
+      { x: 2, y: 3 },
+      { x: 3, y: 5 },
+      { x: 4, y: 4 },
+      { x: 5, y: 7 }
+    ]}
+  />
+</VictoryChart>
         </>
     )
 }

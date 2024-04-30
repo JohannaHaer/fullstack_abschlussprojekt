f906799952fbf0ac0395d3ed1655db29f377da62
VictordelaM <delamotte.v@gmail.com>
2024-04-30 15:34:53 +0200

    checkRepeat gefixed und in editUser eingebunden

--- backend/middleware/checkRepeat.js
+++ backend/middleware/checkRepeat.js
@@ -1,12 +1,17 @@
 import { User } from "../user/userModel/user.model.js";
 
+
+
+//! ES MUSS IMMER ERST NAME DANN EMAIL GECHECKT WERDEN!!!
+//eventuell noch ineinander verschachteln
+
+
 export const checkRepeatName = async (req,res,next)=>{
     const { username } = req.body;
     try {
         const user = await User.findOne({ username }).lean();
         if (user) {
             res.locals.checkNameStatus= "username already exists"
-            res.send('username already exists')
             next()
         } else {
             res.locals.checkNameStatus= "ok"
@@ -24,6 +29,7 @@ export const checkRepeatName = async (req,res,next)=>{
 export const checkRepeatEmail = async(req,res,next)=>{
 
     const { email } = req.body;
+    console.log(res.locals.checkNameStatus)
     try {
         const user = await User.findOne({ email }).lean();
         if (user) {
@@ -31,7 +37,9 @@ export const checkRepeatEmail = async(req,res,next)=>{
             res.send('Email already exists')
         } else {
             res.locals.checkEmailStatus= "ok"
-            next()
+            if(res.locals.checkNameStatus=='username already exists'){
+                res.send('username already exists')
+            }else{next()}
         }
     } catch (error) {
         console.error(error);

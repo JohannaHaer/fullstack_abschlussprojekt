247503bdd606b2b1689670bfa9fa9ee3bd6359f7
VictordelaM <delamotte.v@gmail.com>
2024-05-01 17:35:20 +0200

    addCategory Fetches

--- backend/user/userController/addIncomeCategory.js
+++ backend/user/userController/addIncomeCategory.js
@@ -4,9 +4,8 @@ import { User } from "../userModel/user.model.js";
 
 
 export const addIncomeCategory = async (req,res)=>{
-const username = await jwt.decode(req.cookies.token).username
+    const username = await jwt.decode(req.cookies.token).username
     const {categoryName, color, imgUrl} = req.body
-    console.log('bitte')
         try{
             const user = await User.findOne({username})
         if (!user) {
--- frontend/src/functions/addCategoryFetch.js
+++ frontend/src/functions/addCategoryFetch.js
@@ -1,6 +1,19 @@
+//!not testet
+
+
+
+
+//*es muss ein username im cookies.token gespeichert sein und es können categoryName, color, imgUrl übergeben werden um eine kategorie hinzuzufügen
 export const addIncomeCategory = async(values)=>{
     try{
-
+        const responseIncomeCategory = await fetch(import.meta.env.VITE_BACKEND_URL + '/user/addIncomeCategory', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(values),
+            credentials: 'include'
+        })
     }catch(error){
         console.error(error);
         res.status(500).json({ error: "Internal server error" });
@@ -9,7 +22,14 @@ export const addIncomeCategory = async(values)=>{
 
 export const addExpenseCategory = async(values)=>{
     try{
-
+        const responseExpenseCategory = await fetch(import.meta.env.VITE_BACKEND_URL + '/user/addExpenseCategory', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(values),
+            credentials: 'include'
+        })
     }catch(error){
         console.error(error);
         res.status(500).json({ error: "Internal server error" });

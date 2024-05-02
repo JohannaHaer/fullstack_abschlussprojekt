227609fca3cda920d2af659035c67743533e2490
JohannaHaer <johanna.haerdter@gmail.com>
2024-05-02 17:41:30 +0200

    Add Income und Add Expense haben ihre Gurndfunktionalität und Struktur

--- frontend/src/pages/AddIncome.jsx
+++ frontend/src/pages/AddIncome.jsx
@@ -7,6 +7,7 @@ const AddIncome = () => {
         <>
         <HeaderAddIncome/>
         <section className='py-5 px-4 h-dvh'>
+            <h1 className="text-2xl text-center font-bold pt-10 text-[#0097B2] dark:text-[#FFDE59]">Add Income</h1>
             <AddTransactionForm type={'income'}/>
         </section>
         <NavBar/>

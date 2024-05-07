4f6b3a09d06ce0b2971c1070755cb7bddc23c63a
VictordelaM <delamotte.v@gmail.com>
2024-05-07 10:13:10 +0200

    merge

--- backend/user/userController/uploadImage.js
+++ backend/user/userController/uploadImage.js
@@ -1,13 +1,12 @@
 import { uploadImage } from "../../utils/imgUpload.js";
 import { User } from "../userModel/user.model.js";
-import { userRouter } from "./userLogin.js";
 import jwt from 'jsonwebtoken'
 
 
 
 export const imageUpload = async (req,res) =>{
     try{
-        const username = 'asdfadfadfasf'//jwt.decode(req.cookies.token).username
+        const username = jwt.decode(req.cookies.token).username
         const uploadResult = await uploadImage(req.file.buffer)
         const imgUrl = uploadResult.secure_url
         const user = await User.findOneAndUpdate(
